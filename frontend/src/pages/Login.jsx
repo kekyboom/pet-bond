c2d3f27dafb4ae955c5a9cb36ec32c7d024f2c28
keybel <keybellambertus@gmail.com>
2025-06-16 00:31:05 -0400

    importar Link

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const { login } = useAuth();
